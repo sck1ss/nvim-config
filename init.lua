9eee633c7f4b2a27ca16c708f68f848b309ea08e
z0achary <zacharyxasor@gmail.com>
2024-07-29 14:39:02 +0300

    updated cmp formatting + added lspkind (thanks alisa)

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 require("config.lazy")
 require("opts")
 require("PluginConf.LspConfig")
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,46 @@
 return {
-	"hrsh7th/nvim-cmp",
-	opts = {
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
-			end,
-		}
-	}
+	{
+		"onsails/lspkind.nvim",
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp"
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				formatting = {
+					format = require('lspkind').cmp_format{}
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
